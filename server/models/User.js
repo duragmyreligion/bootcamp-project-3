dbb1e1b18fbfcc1fd9460a1e07d2c93433a6005a
DuragMyReligion <samuelkininmonth@hotmail.com>
2023-11-16 22:10:08 +0800

    Added error msgs to signup page & stronger password validation

--- server/models/User.js
+++ server/models/User.js
@@ -21,10 +21,26 @@ const userSchema = new Schema({
     unique: true
   },
   password: {
-    type: String,
-    required: true,
-    minlength: 5
-  },
+  type: String,
+  required: true,
+  minlength: 8,
+  validate: {
+    validator: function (value) {
+      // Check for at least one lowercase letter
+      const hasLowerCase = /[a-z]/.test(value);
+      // Check for at least one uppercase letter
+      const hasUpperCase = /[A-Z]/.test(value);
+      // Check for at least one digit
+      const hasDigit = /\d/.test(value);
+      // Check for at least one special character or underscore
+      const hasSpecialChar = /[@$!%*?&_]/.test(value);
+  
+      // Combine all checks
+      return hasLowerCase && hasUpperCase && hasDigit && hasSpecialChar;
+    },
+    message: "Password must be 8 characters long and contain one uppercase letter, one lowercase letter, one number, and one special character or underscore."
+  }
+},
   orders: [Order.schema]
 });
 
