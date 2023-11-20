c5b55ee5b20ec63349d770fbb6c3ba4521e0b21e
phil <philippedebiran@gmail.com>
2023-11-20 11:47:04 +0800

    fixed order history page styling

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -30,23 +30,9 @@ const Footer = () => {
           <section className="">
             <form action="">
               <div className="row d-flex justify-content-center">
+
+
                 <div className="col-auto">
-                  <p className="pt-2">
-                    <strong>Sign up for our newsletter</strong>
-                  </p>
-                </div>
-                <div className="col-md-5 col-12">
-                  <div className="form-outline form-white mb-4">
-                    <input type="email" id="form5Example21" className="form-control" />
-                    <label className="form-label" htmlFor="form5Example21">
-                      Email address
-                    </label>
-                  </div>
-                </div>
-                <div className="col-auto">
-                  <button type="submit" className="btn btn-outline-light mb-4">
-                    Subscribe
-                  </button>
                 </div>
               </div>
             </form>
@@ -64,36 +50,48 @@ const Footer = () => {
             <div className="row">
               {/* Repeat this block for each col-lg-3 col-md-6 mb-4 mb-md-0 section */}
               <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
-                <h5 className="text-uppercase">Links</h5>
+                {/* <h5 className="text-uppercase">Links</h5> */}
                 <ul className="list-unstyled mb-0">
                   <li>
                     <a href="#!" className="text-white">
-                      Link 1
+                      Facebook
                     </a>
                   </li>
                   <li>
                     <a href="#!" className="text-white">
-                      Link 2
+                      Instagram
                     </a>
                   </li>
                   <li>
                     <a href="#!" className="text-white">
-                      Link 3
+                      Privacy Policy
                     </a>
                   </li>
                   <li>
                     <a href="#!" className="text-white">
-                      Link 4
+                      Refund Policy
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#!" className="text-white">
+                      Terms of Service
                     </a>
                   </li>
                 </ul>
               </div>
+              <div className="col-md-5 col-12">
+                <ul class = "list-unstyled">
+                  <li>Email: cameron@robusthseq.com.au</li>
+                  <li>Phone: 0408 931 797</li>
+                  
+                  </ul>
+                </div>
             </div>
           </section>
         </div>
   
         <div className="text-center p-3" style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
-          © 2020 Copyright: <a className="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+          © 2020 Copyright: New World Gear  <a className="text-white" href="https://mdbootstrap.com/">Link to something idk</a>
         </div>
       </footer>
     )
